2d872dc55c5cf75716fad66d9a72bac2a4f57ca9
Namra Maniar <namra4122@gmail.com>
2025-01-26 16:03:10 +0530

    mongo retry when fail

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 EXPOSE 80
-CMD ["npm","start"]
+CMD [ "npm", "start" ]
--- src/index.js
+++ src/index.js
@@ -6,12 +6,17 @@ dotenv.config({
     path:"./.env",
 })
 
-connectDB()
+const dbConnect = () =>{
+    connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () =>{
             console.log(`Server Listening at ${process.env.PORT}`);
         }) //if not PORT than 8000
     })
     .catch((err) => {
-        console.error(`MongoDB connection Failed: ${err}`);
+        console.error(`MongoDB connection Failed. Retrying in 3 seconds...: ${err}`);
+        setTimeout(dbConnect, 3000);
     })
+}
+
+dbConnect();
